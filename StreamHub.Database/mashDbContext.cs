650d1e44bf6c16aef7c404d88bc09b43ac6569aa
Phil <pmash2@users.noreply.github.com>
2021-03-03 19:50:36 -0500

    Create initial Database library

---
+++ StreamHub.Database/mashDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StreamHub.Database
+{
+    public class mashDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./mashDbContext.sqlite");
+        }
+    }
+}
