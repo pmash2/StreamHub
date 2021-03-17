65124c555120c7e8cb7ec1b317c47119fa460a2d
Phil <pmash2@users.noreply.github.com>
2021-03-17 19:03:07 -0400

    Create StreamHub.API

---
+++ StreamHub.API/Controllers/ChatMessageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamHub.API.Controllers
+{
+    public class ChatMessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
